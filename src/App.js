fc8e3fed15c7148085957d3014fc7cce2a96b2ec
manuwasa <manuwasa.f@gmail.com>
2023-01-06 14:14:33 +0700

    Finishing project basic layout and routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./component";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./component";
 import "./App.css";
 
 const App = () => {
@@ -11,8 +18,42 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route path={"/"} exact>
+                <Homepage />
+              </Route>
+              <Route path={"/exchanges"} exact>
+                <Exchanges />
+              </Route>
+              <Route path={"/cryptocurrencies"} exact>
+                <Cryptocurrencies />
+              </Route>
+              <Route path={"/crypto/:coinId"} exact>
+                <CryptoDetails />
+              </Route>
+              <Route path={"/news"} exact>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br /> All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/exchanges"}>Exchanges</Link>
+            <Link to={"/news"}>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
