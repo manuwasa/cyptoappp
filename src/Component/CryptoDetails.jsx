35da758cfd617350c6678f891000d624c81794df
manuwasa <manuwasa.f@gmail.com>
2023-01-19 10:18:44 +0700

    Creating CryptoDetails component

--- src/Component/CryptoDetails.jsx
+++ src/Component/CryptoDetails.jsx
@@ -97,9 +97,93 @@ const CryptoDetails = () => {
   ];
 
   return (
-    <>
-      <h1>Cryptodetails</h1>
-    </>
+    <Col className="coin-detail-container">
+      <Col className="coin-heading-header">
+        <Title level={2} className="coin-name">
+          {cryptoDetails.name} {cryptoDetails.slug} Price
+        </Title>
+        <p>
+          {cryptoDetails.name} Live price in US dollar. View value statistics,
+          market cap and supply.
+        </p>
+      </Col>
+      <Select
+        defaultValue={"7d"}
+        className="select-timeperiod"
+        placeholder="Select time period"
+        onChange={(value) => setTimePeriod(value)}
+      >
+        {time.map((date) => {
+          return <Option key={date}>{date}</Option>;
+        })}
+      </Select>
+      {/* line chart */}
+      <Col className="stats-container">
+        <Col className="coin-value-statistics">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              {cryptoDetails.name} Value Statistics
+            </Title>
+            <p>An overview showing the stats of {cryptoDetails.name}</p>
+          </Col>
+          {stats.map(({ icon, title, value }) => {
+            return (
+              <Col className="coin-stats">
+                <Col className="coin-stats-name">
+                  <Text>{icon}</Text>
+                  <Text>{title}</Text>
+                </Col>
+                <Text className="stats">{value}</Text>
+              </Col>
+            );
+          })}
+        </Col>
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              Others Statistics
+            </Title>
+            <p>An overview showing the stats of All cryptocurrencies</p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => {
+            return (
+              <Col className="coin-stats">
+                <Col className="coin-stats-name">
+                  <Text>{icon}</Text>
+                  <Text>{title}</Text>
+                </Col>
+                <Text className="stats">{value}</Text>
+              </Col>
+            );
+          })}
+        </Col>
+      </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails.name} ?
+            {HTMLReactParser(cryptoDetails.description)}
+          </Title>
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links.map((link) => {
+            return (
+              <Row className="coin-link" key={link.name}>
+                <Title level={5} className="link-name">
+                  {link.type}
+                </Title>
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.name}
+                </a>
+              </Row>
+            );
+          })}
+        </Col>
+      </Col>
+    </Col>
   );
 };
 
