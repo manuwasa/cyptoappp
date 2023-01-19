f165bfee078d08011d2ccc5e4ceda2b3052d8a02
manuwasa <manuwasa.f@gmail.com>
2023-01-19 11:14:15 +0700

    Creating chart for CryptoDetails component and Finishing

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "X-RapidAPI-Key": "f5e7a3168amsh3a4a12cb7b18ae8p1d35c7jsn01f4b1f8db75",
-  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+   "X-RapidAPI-Key": "f5e7a3168amsh3a4a12cb7b18ae8p1d35c7jsn01f4b1f8db75",
+   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com/";
@@ -12,27 +12,36 @@ const baseUrl = "https://coinranking1.p.rapidapi.com/";
 // };
 
 export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl, headers: cryptoApiHeaders }),
-  endpoints: (builder) => ({
-    getCryptos: builder.query({
-      query: (arg) => {
-        return {
-          url: "coins",
-          method: "GET",
-          params: `limit=${arg}`,
-        };
-      },
-    }),
-    getCryptoDetails: builder.query({
-      query: (arg) => {
-        return {
-          url: `coin/${arg}`,
-          method: "GET",
-        };
-      },
-    }),
-  }),
+   reducerPath: "cryptoApi",
+   baseQuery: fetchBaseQuery({ baseUrl, headers: cryptoApiHeaders }),
+   endpoints: (builder) => ({
+      getCryptos: builder.query({
+         query: (arg) => {
+            return {
+               url: "coins",
+               method: "GET",
+               params: `limit=${arg}`,
+            };
+         },
+      }),
+      getCryptoDetails: builder.query({
+         query: (arg) => {
+            return {
+               url: `coin/${arg}`,
+               method: "GET",
+            };
+         },
+      }),
+      getCryptoHistory: builder.query({
+         query: (arg) => {
+            return {
+               url: `coin/${arg.coinId}/history`,
+               method: "GET",
+               params: `timePeriod=${arg.timePeriod}`,
+            };
+         },
+      }),
+   }),
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
